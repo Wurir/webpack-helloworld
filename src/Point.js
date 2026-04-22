10adf099e764309579249267bd85374d27d43ca5
WurirMacOs <wurir1994@gmail.com>
2026-04-22 23:12:54 +0200

    exporting/importing class via es6 export default

--- src/Point.js
+++ src/Point.js
@@ -1,10 +1,6 @@
-class Point {
+export default class Point {
     constructor(x, y){
         this.x = x
         this.y = y
     }
 }
-
-module.exports = Point
-
-//eksportujac plik zawierajacy klase, plik powinien nazywac sie z duzej litery (tak jak klasa, np. Point.js)
--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import getCurrentDate from './getDate.js' // importowanie plikow przez przeglądarke wymaga podania rozszerzenia pliku np. (.js)
+import Point from './Point.js'
 
 const date = getCurrentDate()
 
 console.log(date.getFullYear());
+
+const p1 = new Point(1, 2)
+
+console.log(p1);
